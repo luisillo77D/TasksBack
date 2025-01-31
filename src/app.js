775e617a1e23ca46ce10dd7276d2d78d1cd41f0e
luisillo77D <luisillo77mario@gmail.com>
2025-01-30 22:18:57 -0700

    feat(auth): update logout to expire token cookie and enhance CORS configuration with sameSite attribute

--- src/app.js
+++ src/app.js
@@ -11,6 +11,7 @@ app.use(cors(
     {
         origin: ["http://localhost:5173","https://prueba-vue-beta.vercel.app"],
         credentials: true,
+        sameSite: "none"
     }
 ));
 app.use(cookieParser());
