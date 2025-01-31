8c55adc7d83bea961c4c6a70a015848859f39bb2
luisillo77D <luisillo77mario@gmail.com>
2025-01-30 21:51:00 -0700

    feat(cors): restrict CORS to specific origins for enhanced security

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: "*",
+        origin: ["http://localhost:5173","https://prueba-vue-beta.vercel.app"],
         credentials: true,
     }
 ));
