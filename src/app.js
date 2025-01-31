b74bea24dd55c30b1e412f4efbd7677a788ef919
luisillo77D <luisillo77mario@gmail.com>
2025-01-30 21:48:02 -0700

    xdd

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ const app = express();
 
 app.use(cors(
     {
-        origin: ["http://localhost:5173","https://prueba-vue-beta.vercel.app/"],
+        origin: "*",
         credentials: true,
     }
 ));
